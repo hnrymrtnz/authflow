d31e81451ab748383b92157352582cb66207b6c4
hnrymrtnz <henry.martinez.713@my.csun.edu>
2024-11-25 17:42:40 -0800

    Modified files

--- app/openid_connect/routes.py
+++ app/openid_connect/routes.py
@@ -5,6 +5,10 @@ from flask import render_template
 def auth_code_flow():
     return render_template('openid_connect/auth_code.html')
 
+@openid_connect.route('/auth_code/auth')
+def auth_code_flow_auth():
+    return render_template('openid_connect/auth_code/auth.html')
+
 @openid_connect.route('/implicit')
 def implicit_flow():
     return render_template('openid_connect/implicit.html')
