8e1660fef1bfc61d73d556935f5bb2982d908260
hnrymrtnz <henry.martinez.713@my.csun.edu>
2024-12-15 14:36:43 -0800

    Added changes

--- app/openid_connect/routes.py
+++ app/openid_connect/routes.py
@@ -3,6 +3,8 @@ from config import *
 from flask import render_template, redirect, request, session
 import requests, jwt
 
+#########################################################################################################################
+
 @openid_connect.route('/auth_code')
 def auth_code_flow():
     return render_template('auth_code.html')
@@ -12,12 +14,12 @@ def auth_code_flow_auth():
     try:
         issuer = ISSUER
     except:
-        return 'Missing OpenID provider issuer URL. Update the "config.cfg" file and try again.'
+        return 'Missing OpenID provider issuer URL. Update the "config.py" file and try again.'
 
     auth_endpoint = AUTHORIZATION_ENDPOINT
-    redirect_uri = REDIRECT_URI
-    client_id = CLIENT_ID
-    scopes = SCOPES
+    redirect_uri = AC_REDIRECT_URI
+    client_id = AC_CLIENT_ID
+    scopes = AC_SCOPES
 
     auth_url = auth_endpoint + \
         '?redirect_uri=' + redirect_uri + \
@@ -39,8 +41,8 @@ def auth_code_flow_token():
     token_url = TOKEN_ENDPOINT
 
     data = {'grant_type':'authorization_code', 'code':session['code'], \
-        'redirect_uri':REDIRECT_URI, 'client_id':CLIENT_ID, \
-        'client_secret':CLIENT_SECRET}
+        'redirect_uri':AC_REDIRECT_URI, 'client_id':AC_CLIENT_ID, \
+        'client_secret':AC_CLIENT_SECRET}
 
     response = requests.post(token_url, data, verify=False)
     response = response.json()
@@ -55,11 +57,42 @@ def auth_code_flow_claims():
     claims = jwt.decode(session['access_token'], options={"verify_signature": False})
 
     return render_template('auth_code/claims.html', claims=claims)
-##############################################
+
+#########################################################################################################################
 
 @openid_connect.route('/implicit')
 def implicit_flow():
-    return render_template('openid_connect/implicit.html')
+    return render_template('implicit.html')
+
+@openid_connect.route('/implicit/auth')
+def implicit_flow_auth():
+    try:
+        issuer = ISSUER
+    except:
+        return 'Missing OpenID provider issuer URL. Update the "config.py" file and try again.'
+
+    auth_endpoint = AUTHORIZATION_ENDPOINT
+    redirect_uri = I_REDIRECT_URI
+    client_id = I_CLIENT_ID
+    scopes = I_SCOPES
+
+    auth_url = auth_endpoint + \
+        '?redirect_uri=' + redirect_uri + \
+        '&client_id=' + client_id + \
+        '&response_type=token' + \
+        '&scope=' + scopes 
+
+    return redirect(auth_url)
+
+@openid_connect.route('/implicit/callback')
+def implicit_flow_callback():
+    session['access_token'] = request.args.get('access_token')
+    session['id_token'] = request.args.get('id_token')
+    session['session_state'] = request.args.get('session_state')
+
+    return render_template('implicit/callback.html', access_token=session['access_token'], id_token=session['id_token'], state=session['session_state'])
+
+#########################################################################################################################
 
 @openid_connect.route('/client_cred')
 def hybrid_flow():
--- config.py
+++ config.py
@@ -2,10 +2,19 @@ SECRET_KEY = 'N3n2-bJko1n49Am2-J015I'
 
 # OpenID Connect Keycloak Configuration
 ISSUER = 'http://localhost:8080/realms/master'
-CLIENT_ID = 'authflow-client'
-CLIENT_SECRET = 'NYKmCduux6hPI60dLPiFoIQ5jwpnehoc'
-SCOPES = 'openid'
-REDIRECT_URI = 'http://localhost:5000/openid_connect/auth_code/callback'
 AUTHORIZATION_ENDPOINT = 'http://localhost:8080/realms/master/protocol/openid-connect/auth'
 TOKEN_ENDPOINT = 'http://localhost:8080/realms/master/protocol/openid-connect/token'
 USERINFO_ENDPOINT = '"http://localhost:8080/realms/master/protocol/openid-connect/userinfo'
+
+# Authorization Code Flow
+AC_CLIENT_ID = 'authflow-auth_code'
+AC_CLIENT_SECRET = 'NYKmCduux6hPI60dLPiFoIQ5jwpnehoc'
+AC_SCOPES = 'openid'
+AC_REDIRECT_URI = 'http://localhost:5000/openid_connect/auth_code/callback'
+
+# Implicit Grant Flow
+I_CLIENT_ID = 'authflow-implicit'
+I_CLIENT_SECRET = 'ccw31u7PtazDXqgTt3UDA8al6xGKUhKg'
+I_SCOPES = 'openid'
+I_REDIRECT_URI = 'http://localhost:5000/openid_connect/implicit/callback'
+
