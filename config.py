474a7ceff3d16eb04ba5010a9675f58d42b07890
hnrymrtnz <henry.martinez.713@my.csun.edu>
2024-12-15 17:09:18 -0800

    Added new redirect URIs

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ USERINFO_ENDPOINT = 'https://dev-efxkhtkp2616hoy5.us.auth0.com/userinfo'
 AC_CLIENT_ID = '1XBtyViD2SOHwfbmLFH5wU4EIDP80JDX'
 AC_CLIENT_SECRET = 'h_2vGN5sxwl24zOaGZfM9csL-uu7bpwTEuqJckO17tcX5YW3tgrWE7NLK4YkPnyO'
 AC_SCOPES = 'openid profile'
-AC_REDIRECT_URI = 'http://localhost:5000/openid_connect/auth_code/callback'
+AC_REDIRECT_URI = 'https://authflow-4d76684d9ec6.herokuapp.com/openid_connect/auth_code/callback'
 
 # Implicit Grant Flow
 I_CLIENT_ID = 'uNMHvwCmP9l7iPGIayptGzjftoHE23Lw'
 I_CLIENT_SECRET = 'os5Y2ojOzdEVexOVwOED3nDGpbj6RxhBmEQrogDlTPBYMDVgZr2bznRGYSfHu3u4'
 I_SCOPES = 'openid profile'
-I_REDIRECT_URI = 'http://localhost:5000/openid_connect/implicit/callback'
+I_REDIRECT_URI = 'https://authflow-4d76684d9ec6.herokuapp.com/openid_connect/implicit/callback'
 
